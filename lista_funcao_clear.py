ad10be7c858e6897e7b27afc9a2cb67e8eeace2b
FabiABt <fabiane489@gmail.com>
2022-06-07 19:36:47 -0300

    clear

---
+++ lista_funcao_clear.py
@@ -0,0 +1,8 @@
+#lista de animais 
+# clear-(Elemento)- Realiza a limpeza de todos os nodos de uma lista. Essa limpeza libera o espaço ocupado na memória do computador. Uma vez executada, os
+# dados não poderão ser recuperados.
+animais = ['gato', 'cachorro', 'papagaio', 'gato']
+#imprime a nova lista 
+print(animais)
+#limpa os elementos da lista
+animais.clear
