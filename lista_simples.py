d57f4e846540e44eeb735242df60aa8fc89f5fdc
FabiABt <fabiane489@gmail.com>
2022-06-07 19:33:40 -0300

    count-(Elemento)

--- lista_simples.py
+++ lista_simples.py
@@ -1,1 +1,5 @@
-sim
+#lista de animais 
+# count-(Elemento)- Informa a quantidade de elementos enviados por parâmetro que existem na variável Lista
+animais = ['gato', 'cachorro', 'papagaio', 'gato']
+#imprime a nova lista 
+print(animais.count('gato'))
