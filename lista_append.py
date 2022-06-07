524159a8553d84d881633b1599e5b999173b433c
FabiABt <fabiane489@gmail.com>
2022-06-07 20:13:01 -0300

    insert

---
+++ lista_append.py
@@ -0,0 +1,7 @@
+#lista de animais 
+# append-(Elemento)- Adiciona a variável elemento ao final da lista.
+animais = ['gato', 'cachorro', 'papagaio', 'gato']
+# adiciona coelho no final da lista 
+animais.append('coelho')
+#imprime a nova lista 
+print(animais)
---
+++ lista_insert.py
@@ -0,0 +1,9 @@
+#lista de animais 
+# insert-(Posicao,Elemento)- Adiciona o elemento em uma posição específica de uma lista.
+animais = ['gato', 'cachorro', 'papagaio', 'gato']
+#imprime a  lista 
+print(animais)
+# insere o elemento rato na posicao 1 da lista 
+animais.insert(1,'rato')
+#imprime a  lista 
+print(animais)
