5562e8f1694401ae5e74a9bd51cefdb584a0b09e
FabiABt <fabiane489@gmail.com>
2022-06-07 19:24:14 -0300

    .

---
+++ lista_encadeada_simples.py
@@ -0,0 +1,12 @@
+# criacao das classes 
+#classe 
+class produto:
+    def__init__(self, codigo = "indefinido", peso = 0):
+      self.codigo = codigo
+      self.tipo = peso
+    def__repr__(self)
+      retur'%s->%d'%(self.codigo, self.tipo)  
+#classe Nodo
+class nodo:
+    def__init__(self, dado=0, proximo_nodo=none):
+        print()
